a69ddef795aa76974414a7ddf7996f551cb82af7
Fatumayattani <fyattani@gmail.com>
2025-01-30 12:36:51 +0300

    save changes

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",  // add paths to your files where Tailwind classes will be used
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
